677946626c09031465af9ab2bc1a296c549c0543
Benoît <benoit.husson.tours@gmail.com>
2020-09-15 10:14:26 +0200

    test 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -45,6 +45,7 @@ class HomePage extends React.Component {
                     soucing our produce locally and you'll always 
                     find the season on our menu. We especially love tropical produce 
 <<<<<<< HEAD
+<<<<<<< HEAD
                     and we're always featuring the best of the tropics"
                     imageSectionBloc={imageTarteFlambee}
                     linkSectionBloc="Unsere Feintkostladen entdecken"
@@ -54,6 +55,11 @@ class HomePage extends React.Component {
                     imageSectionBloc={imageTarteFlambee}
                     linkSectionBloc="Unsere Feintkostladen entdecken."
 >>>>>>> parent of 458e88c... Revert "Changes for git revert training"
+=======
+                    and we're always featuring the best of the tropics !"
+                    imageSectionBloc={imageTarteFlambee}
+                    linkSectionBloc="Unsere Feintkostladen entdecken !"
+>>>>>>> parent of cbf7fd3... test
                     test="../../assets/images/background_bloc_section.jpg"
                 />
             </div>
